805229151f51882098ced22dce281016f95266aa
Juvenal Carlos Cardoso <juvenalcardoso961@gmail.com>
2022-09-09 17:53:07 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notefication-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notefication-icon.svg';
+import './styles.css';
 function NotificationButton() {
     return(
       <div className="dsmeta-red-btn">
@@ -11,5 +11,5 @@ function NotificationButton() {
     )
   }
   
-  export default NotificationButton
+  export default NotificationButton;
   
