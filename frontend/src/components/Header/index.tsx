9ae8f9ed1838dc1373ca1c3573344c2745da4602
Juvenal Carlos Cardoso <juvenalcardoso961@gmail.com>
2022-09-08 21:34:33 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+  return(
+    
+    <header>
+        <div className="dsmete-logo-container">
+            <img src={logo} alt="DSMete"/>
+            <h1>DSMeta</h1>
+                <p>
+                  Desenvolvido por 
+                  <a href="https://github.com/CarlosCardosoSilva/CarlosCardoso.git">@devsuperior.ig</a>
+               </p>
+        </div>
+        
+    </header>
+    
+  )
+}
+
+export default Header
